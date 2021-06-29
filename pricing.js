5afb2946515809828beebf5f4b79daebe0018db7
heraldsandoy <sandoyherald1993@gmail.com>
2021-06-29 23:57:49 +0800

    Update pricing.js

--- pricing.js
+++ pricing.js
@@ -2,21 +2,20 @@
         function () {         
          
                $("#node-box-1 .img-chip")
-               .fadeOut(200, function() {
+               .fadeIn(600, function() {
                    $("#node-box-1 .img-chip").prop('src',"https://uploads-ssl.webflow.com/5e4cf0c7e19c924b448d0560/5ee657d5b71cb46d0b7869fa_blue-processor.gif");
                })
-               .fadeIn(200);
+              
          
                 $("#node-box-1 .price-node-holder").css("color", "#68a9d8"),
                 $("#node-box-1 .price-node-holder span").css("color", "#68a9d8");
         },
         function () {       
          
-//                $("#node-box-1 .img-chip")
-//                .fadeIn(300, function() {
-//                    $("#node-box-1 .img-chip").prop("src", "https://uploads-ssl.webflow.com/5e4cf0c7e19c924b448d0560/5e7e1db76504fe23c40f14c6_Vector-2.png");
-//                })
-//                .fadeIn(500);
+               $("#node-box-1 .img-chip")
+               .fadeIn(600, function() {
+                   $("#node-box-1 .img-chip").prop("src", "https://uploads-ssl.webflow.com/5e4cf0c7e19c924b448d0560/5e7e1db76504fe23c40f14c6_Vector-2.png");
+               }).fadeOut(600)
          
            
                 $("#node-box-1 .price-node-holder").prop("style", "color:rgb(102, 102, 102)"),
