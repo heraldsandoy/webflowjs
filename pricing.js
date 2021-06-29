eceb2d261f75cb8394d1e215726b6c830b515c65
heraldsandoy <sandoyherald1993@gmail.com>
2021-06-29 23:50:20 +0800

    Update pricing.js

--- pricing.js
+++ pricing.js
@@ -2,10 +2,10 @@
         function () {         
          
                $("#node-box-1 .img-chip")
-               .fadeIn(500, function() {
+               .fadeOut(200, function() {
                    $("#node-box-1 .img-chip").prop('src',"https://uploads-ssl.webflow.com/5e4cf0c7e19c924b448d0560/5ee657d5b71cb46d0b7869fa_blue-processor.gif");
                })
-               .fadeIn(300);
+               .fadeIn(200);
          
                 $("#node-box-1 .price-node-holder").css("color", "#68a9d8"),
                 $("#node-box-1 .price-node-holder span").css("color", "#68a9d8");
